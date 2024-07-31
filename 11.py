512ca70254dda706c6f63365765984f4f3a0b36e
harshsbhat <icybhat@gmail.com>
2024-07-31 10:27:43 +0530

    Container with most area

---
+++ 11.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height)-1
+        area = 0
+        while l < r:
+            area = max((min(height[l], height[r]) * (r-l)), area)
+            if height[l] > height[r]:
+                r -=  1
+            else: 
+                l += 1
+        return area
+
